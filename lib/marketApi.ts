10d21a31440c787e939939b69f6dce451f7bc387
Hiroki Chiyoda <contact@arafipro.com>
2023-07-16 08:06:06 +0900

    marketsテーブルのCRUDを実装#11

--- lib/marketApi.ts
+++ lib/marketApi.ts
@@ -9,3 +9,66 @@ export async function getAllMarkets() {
   }
   return res.json();
 }
+
+export async function getMarket(id: number) {
+  const res = await fetch(`${BaseUrl}/${id}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    console.log(res);
+  }
+  return res.json();
+}
+
+export async function addMarket(market: Market): Promise<Market> {
+  if (window.confirm("Are you sure to create this record?")) {
+    const res = await fetch(`${BaseUrl}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        market_id: market.market_id,
+        market: market.market,
+      }),
+    });
+    const newMarket: Market = await res.json();
+    return newMarket;
+  } else {
+    return Promise.reject("User canceled the operation.");
+  }
+}
+
+export async function updateMarket(market: Market): Promise<Market> {
+  if (window.confirm("Are you sure to create this record?")) {
+    const res = await fetch(`${BaseUrl}/${market.market_id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        market_id: market.market_id,
+        market: market.market,
+      }),
+    });
+    const updateMarket = await res.json();
+    return updateMarket;
+  } else {
+    return Promise.reject("User canceled the operation.");
+  }
+}
+
+export async function delMarket(id: number): Promise<void> {
+  if (window.confirm("Are you sure to delete this record?")) {
+    const res = await fetch(`${BaseUrl}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const deletedMarket = await res.json();
+    return deletedMarket;
+  } else {
+    return Promise.reject("User canceled the operation.");
+  }
+}
