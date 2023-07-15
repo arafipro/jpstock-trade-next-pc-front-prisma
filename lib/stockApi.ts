9d149d890bff7e7608bae58dd82d9ed522e0d4b7
Hiroki Chiyoda <contact@arafipro.com>
2023-07-16 08:38:38 +0900

    stocksテーブルのCRUDを実装#14

--- lib/stockApi.ts
+++ lib/stockApi.ts
@@ -9,3 +9,68 @@ export async function getAllStocks() {
   }
   return res.json();
 }
+
+export async function getStock(id: number) {
+  const res = await fetch(`${BaseUrl}/${id}`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    console.log(res);
+  }
+  return res.json();
+}
+
+export async function addStock(stock: Stock): Promise<Stock> {
+  if (window.confirm("Are you sure to create this record?")) {
+    const res = await fetch(`${BaseUrl}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: stock.code,
+        stockname: stock.stockname,
+        market_id: stock.market_id,
+      }),
+    });
+    const newStock: Stock = await res.json();
+    return newStock;
+  } else {
+    return Promise.reject("User canceled the operation.");
+  }
+}
+
+export async function updateStock(stock: Stock): Promise<Stock> {
+  if (window.confirm("Are you sure to create this record?")) {
+    const res = await fetch(`${BaseUrl}/${stock.code}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: stock.code,
+        stockname: stock.stockname,
+        market_id: stock.market_id,
+      }),
+    });
+    const updateStock = await res.json();
+    return updateStock;
+  } else {
+    return Promise.reject("User canceled the operation.");
+  }
+}
+
+export async function delStock(id: number): Promise<void> {
+  if (window.confirm("Are you sure to delete this record?")) {
+    const res = await fetch(`${BaseUrl}/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const deletedStock = await res.json();
+    return deletedStock;
+  } else {
+    return Promise.reject("User canceled the operation.");
+  }
+}
