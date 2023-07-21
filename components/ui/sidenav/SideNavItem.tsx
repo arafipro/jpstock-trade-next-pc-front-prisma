f79227433538c3aa649fb882fbc73d5ec6b8fd92
Hiroki Chiyoda <contact@arafipro.com>
2023-07-21 17:51:43 +0900

    バグを修正

--- components/ui/sidenav/SideNavItem.tsx
+++ components/ui/sidenav/SideNavItem.tsx
@@ -3,20 +3,16 @@ import Link from "next/link";
 const SideNavItem = ({
   children,
   href,
-  mb = 4,
-  pl = 4,
 }: {
   children: React.ReactNode;
   href: string;
-  mb?: number;
-  pl?: number;
 }) => {
   return (
     <div>
-      <li className={`mb-${mb}`}>
+      <li className="mb-4">
         <Link
           href={href}
-          className={`text-gray-500 hover:text-black hover:font-bold hover:tracking-wider duration-100 pl-${pl}`}
+          className="text-gray-500 hover:text-black hover:font-bold hover:tracking-wider duration-100 pl-4"
         >
           {children}
         </Link>
