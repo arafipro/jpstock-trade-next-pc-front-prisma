4e3656df2f7b6cc7ad83d7717f77b6db31ae01db
Hiroki Chiyoda <contact@arafipro.com>
2023-07-21 19:26:21 +0900

    フォントNoto Sans JPを適用#25

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
 import HeadNavBar from "@/components/HeadNavBar";
 import SideNavBar from "@/components/SideNavBar";
 import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
+
+const notosanjp = Noto_Sans_JP({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: " jpstock-trade-next-pc-front",
@@ -14,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja">
+    <html lang="ja" className={notosanjp.className}>
       <body className="flex w-screen bg-gray-100">
         <SideNavBar />
         <div className="w-full">
